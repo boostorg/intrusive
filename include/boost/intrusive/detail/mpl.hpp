650ecfa4c343cead3162916ba58cffe93159d244
Ion Gaztañaga <igaztanaga@gmail.com>
2014-05-11 14:23:20 +0200

    Tidied copyright notice

--- include/boost/intrusive/detail/mpl.hpp
+++ include/boost/intrusive/detail/mpl.hpp
@@ -1,7 +1,8 @@
 /////////////////////////////////////////////////////////////////////////////
 //
-// (C) Copyright Ion Gaztanaga  2006-2013
-// Copyright (c) Microsoft Corporation
+// (C) Copyright Ion Gaztanaga  		2006-2014
+// (C) Copyright Microsoft Corporation  2014
+//
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
