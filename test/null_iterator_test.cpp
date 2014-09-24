5c30cd107260bf0b28730104a045d537f20a2b64
Ion Gaztañaga <igaztanaga@gmail.com>
2014-09-24 15:30:57 +0200

    Fixed copy-paste error from copyright.

--- test/null_iterator_test.cpp
+++ test/null_iterator_test.cpp
@@ -1,6 +1,5 @@
 //////////////////////////////////////////////////////////////////////////////
 //
-// (C) Copyright Howard Hinnant 2014.
 // (C) Copyright Ion Gaztanaga 2014-2014. Distributed under the Boost
 // Software License, Version 1.0. (See accompanying file
 // LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
