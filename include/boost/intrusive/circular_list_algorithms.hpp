2f4ee6bdcf0fec5b1a62bc3de2d78567a6dfc98e
Joerg Sonnenberger <joerg@bec.de>
2015-01-13 12:18:26 +0100

    Don't use plain throw to unbreak clang -fno-exceptions

--- include/boost/intrusive/circular_list_algorithms.hpp
+++ include/boost/intrusive/circular_list_algorithms.hpp
@@ -421,7 +421,7 @@ class circular_list_algorithms
             BOOST_CATCH(...){
                node_traits::set_next    (last_to_remove, new_f);
                node_traits::set_previous(new_f, last_to_remove);
-               throw;
+               BOOST_RETHROW
             }
             BOOST_CATCH_END
             node_traits::set_next(last_to_remove, new_f);
