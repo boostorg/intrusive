2cdcf4555975cbe21f4a0a4eda86e437f5742252
Ion Gaztañaga <igaztanaga@gmail.com>
2026-09-18 22:03:26 +0200

    Remove launder (compilers still respect the arithmetic without launder, which makes performance worse on GCC) and make void * cast to avoid cast-align warnings

--- include/boost/intrusive/detail/parent_from_member.hpp
+++ include/boost/intrusive/detail/parent_from_member.hpp
@@ -91,15 +91,15 @@ BOOST_INTRUSIVE_FORCEINLINE std::ptrdiff_t offset_from_pointer_to_member(const M
 template<class Parent, class Member>
 BOOST_INTRUSIVE_FORCEINLINE Parent *parent_from_member(Member *member, const Member Parent::* ptr_to_member)
 {
-   return boost::move_detail::launder(reinterpret_cast<Parent*>
-      (reinterpret_cast<std::size_t>(member) - static_cast<std::size_t>(offset_from_pointer_to_member(ptr_to_member))));
+   return static_cast<Parent*>(static_cast<void*>
+      (reinterpret_cast<char*>(member) - offset_from_pointer_to_member(ptr_to_member)));
 }
 
 template<class Parent, class Member>
 BOOST_INTRUSIVE_FORCEINLINE const Parent *parent_from_member(const Member *member, const Member Parent::* ptr_to_member)
 {
-   return boost::move_detail::launder(reinterpret_cast<const Parent*>
-      ( reinterpret_cast<std::size_t>(member) - static_cast<std::size_t>(offset_from_pointer_to_member(ptr_to_member)) ));
+   return static_cast<const Parent*>(static_cast<const void*>
+      (reinterpret_cast<const char*>(member) - offset_from_pointer_to_member(ptr_to_member)));
 }
 
 }  //namespace detail {
